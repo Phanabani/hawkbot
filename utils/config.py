8e1364a148cd5aecd600006ff441df485f0c3886
Hawkpath <hawkpathas@gmail.com>
2020-01-15 15:19:28 -0500

    Environment properties in the config now take precedence over global properties with the same names

--- utils/config.py
+++ utils/config.py
@@ -8,12 +8,16 @@ class Config:
     def __init__(self, file):
         with open(file) as f:
             self._config = json.load(f)
-            self.env = self._config['env']
+        self.env_name = self._config['env']
+        self._env = self._config['envs'][self.env_name]
 
     def __getitem__(self, item):
-        if item not in self._config:
-            return self._config['envs'][self.env][item]
+        if item in self._env:
+            return self._env[item]
         return self._config[item]
+
+    def __contains__(self, item):
+        return item in self._env or item in self._config
 
 
 config = Config(DATA_FOLDER / 'config.json')
